3450e22a0b069912e50b9fc77d4d0ff5d97b4df5
StephAdongo <stephanie@kabarak.ac.ke>
2025-05-26 19:09:28 +0300

    Enhance responsive design

---
+++ memory-card/src/components/game/Card.jsx
@@ -0,0 +1,29 @@
+import {useState, useEffect } from 'react';
+import './Card.css';
+
+export default function Card({ character, onSelect, isFlipping }) {
+const [isFlipped, setIsFlipped] = useState(false);
+
+useEffect(() => {
+    setIsFlipped(isFlipping);
+}, [isFlipping]);
+
+const handleClick = () => {
+    setTimeout(() => {
+        onSelect(character.id);
+    }, 500);
+};
+return (
+    <div className={`card-container ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+        <div className="card">
+            <div className="card-front">
+                <img src={character.image || '/placeholder.png'} alt={character.name} />
+                <h3>{character.name}</h3>
+            </div>
+            <div className="card-back">
+                <img src="/images/logo.png" alt ="Card Back" />
+            </div>
+        </div>
+    </div>
+);
+}
