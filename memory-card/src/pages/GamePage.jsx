d32a093fe5b962f7f15e5a5f915f29203d046b61
StephAdongo <stephanie@kabarak.ac.ke>
2025-05-27 11:00:25 +0300

    Game page customization

---
+++ memory-card/src/pages/GamePage.jsx
@@ -0,0 +1,34 @@
+import { useGame } from '../context/GameContext';
+import Header from '../components/layout/Header';
+import GameBoard from '../components/game/GameBoard';
+import GameModal from '../components/ui/GameModal';
+import './GamePage.css';
+import SettingsPanel from '../components/ui/SettingsPanel';
+import HelpPanel from '../components/ui/HelpPanel';
+import StartScreen from '../components/ui/StartScreen';
+
+export default function GamePage() {
+    const { gameStarted } = useGame();
+
+    return (
+        <div className="game-page">
+        <div className="game-background"></div>
+        <div className="game-container">
+          {gameStarted && <Header />}
+          
+          <div className="game-area">
+            {!gameStarted ? (
+              <StartScreen />
+            ) : (
+              <GameBoard />
+            )}
+          </div>
+          
+          <GameModal />
+          <SettingsPanel />
+          <HelpPanel />
+        </div>
+      </div>
+    );
+  } 
+    
